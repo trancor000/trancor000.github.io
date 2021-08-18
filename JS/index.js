dc8effe0a721904d90c221e5d11784740739e118
trancor000 <83535306+trancor000@users.noreply.github.com>
2021-08-18 12:37:32 -0700

    Update

--- JS/index.js
+++ JS/index.js
@@ -23,7 +23,6 @@ function currentSlide(n) {
 
 
 function showSlides(n) {
-    var i;
     var slides = document.getElementsByClassName("mySlides"); // This takes all elements with the class name "mySlides" and stores them in the variable array "slides"
     var dots = document.getElementsByClassName("dot"); // This takes all elements with the class name "dot" and stores them in the variable array "dots"
     if (n > slides.length) { slideIndex = 1 }; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
@@ -34,6 +33,6 @@ function showSlides(n) {
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", ""); // This for loop takes each item in the array "dots" and removes "active" which removes the active styling
     }
-    slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
+    slides[slideIndex - 1].style.display = "active"; // This displays the image in the slideshow
     dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
 }
