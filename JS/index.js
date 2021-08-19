50ce557b25dac1960e6b3ffb6a5c66ec6327b9c2
trancor000 <83535306+trancor000@users.noreply.github.com>
2021-08-19 08:30:58 -0700

    Update Index.js

--- JS/index.js
+++ JS/index.js
@@ -33,6 +33,6 @@ function showSlides(n) {
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", ""); // This for loop takes each item in the array "dots" and removes "active" which removes the active styling
     }
-    slides[slideIndex - 1].style.display = "active"; // This displays the image in the slideshow
+    slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
     dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
 }
